a97f2f0e986099c2402771372c0eb952f33e368b
CheeZe Dark(Rikko Matsumato) <cheezedarkofficial@gmail.com>
2025-10-04 02:33:50 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 import DownloadEAFC24 as d_eafc24
 
 def Main():
-    url_origineafc24 = "https://prod.cloudflare.cdn.ea.com/published/eamaster/s/shift/fc/fc_24/patch__ww_x0_patchb/fc_24pcpatch__ww_x0_patchbconcept_125__phoenixwwretail78826429235397a16794809bf7565700ce1bf79.zip?verify=1758006779-kC6K6zs%2BnqE1Nb6pOCE%2Fq0gqjzbFHK1jN2aeIff69do%3D"
+    url_origineafc24 = "https://origin-a.akamaihd.net/eamaster/s/shift/fc/fc_24/patch__ww_x0_patchb/fc_24pcpatch__ww_x0_patchbconcept_125__phoenixwwretail78826429235397a16794809bf7565700ce1bf79.zip?sauth=1759620766_003e2a00714ee8a6dc2d7f0cbec36196"
     output = "EAFC24_PCFullVersion.zip"
     d_eafc24.DownloadEAFC24(url=url_origineafc24, output_path=output)
 
 if __name__ == "__main__":
     Main()
+
